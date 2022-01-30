f0c3160e4ffa3df01cc37101403ef85377cad3c6
Juliana R Fernandes <littlejuh@live.it>
2022-01-30 16:51:13 -0300

    :white_check_mark: test execute kill line in logline service

--- spec/lib/quake_log_parser/services/logline_service_spec.rb
+++ spec/lib/quake_log_parser/services/logline_service_spec.rb
@@ -22,14 +22,14 @@ describe QuakeLogParser::LoglineService do
   end
 
   context '#execute kill line' do
+    let(:kill_line) do
+      '22:06 Kill: 2 3 7: Isgalamido killed Mocinha by MOD_ROCKET_SPLASH'
+    end
     let(:game) { double(QuakeLogParser::Game) }
     let(:killer_name) { 'Isgalamido' }
     let(:killer_player) { double(QuakeLogParser::Player) }
     let(:killed_player) { double(QuakeLogParser::Player) }
     let(:kill) { double(QuakeLogParser::Kill) }
-    let(:kill_line) do
-      '22:06 Kill: 2 3 7: Isgalamido killed Mocinha by MOD_ROCKET_SPLASH'
-    end
 
     before do
       allow(log).to receive(:current_game).and_return(game)
@@ -45,6 +45,29 @@ describe QuakeLogParser::LoglineService do
       expect(game).to receive(:add_player).with(player: killed_player)
       expect(game).to receive(:add_kill).with(kill: kill)
       expect(killer_player).to receive(:score_point)
+      expect(killed_player).to receive(:downscore)
+      expect(subject.execute(logline: kill_line)).to eql(log)
+    end
+  end
+
+  context '#execute kill by world line' do
+    let(:kill_line) do
+      '20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT'
+    end
+    let(:game) { double(QuakeLogParser::Game) }
+    let(:killed_player) { double(QuakeLogParser::Player) }
+    let(:kill) { double(QuakeLogParser::Kill) }
+
+    before do
+      allow(log).to receive(:current_game).and_return(game)
+      allow(QuakeLogParser::Player).to receive(:new).with(name: 'Isgalamido').and_return(killed_player)
+      allow(QuakeLogParser::Kill).to receive(:new).with(killer: nil, killed: killed_player,
+                                                        cause: 'MOD_TRIGGER_HURT').and_return(kill)
+    end
+
+    it 'should add kill in current game' do
+      expect(game).to receive(:add_player).with(player: killed_player)
+      expect(game).to receive(:add_kill).with(kill: kill)
       expect(killed_player).to receive(:downscore)
       expect(subject.execute(logline: kill_line)).to eql(log)
     end
